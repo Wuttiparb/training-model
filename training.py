8a92faf5e7b43b2ce3186e11405515e4a6207f4e
Wuttiparb <mizsizterx@gmail.com>
2025-06-13 10:40:57 +0700

    Update training.py

--- training.py
+++ training.py
@@ -3,6 +3,7 @@ import os
 from roboflow import Roboflow
 from ultralytics import YOLO
 import torch
+import time
 
 def main():
     print(torch.cuda.is_available())
@@ -21,6 +22,11 @@ def main():
         plots=True,
         device=0  # 0 for first GPU, 1 for second, 'cpu' for CPU
     )
+def runtime():
+    start = time.time()
+    main()
+    end = time.time()
+    print(f'Runtime: {end-start:.3f} second')
 
 if __name__ == "__main__":
-    main()
+    runtime()
