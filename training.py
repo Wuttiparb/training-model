aa5e3cdb92f19d23fd1a16165f080be8e05ed71f
Wuttiparb <mizsizterx@gmail.com>
2025-06-18 02:33:22 +0700

    trrain

--- training.py
+++ training.py
@@ -9,18 +9,18 @@ def main():
     print(torch.cuda.is_available())
     ultralytics.checks()
 
-    rf = Roboflow(api_key="EHBMka1dvuHyFvLJmoo2")
-    project = rf.workspace("cascade-label").project("cassette-trackingg")
-    version = project.version(6)
-    dataset = version.download("yolov11")
 
+    rf = Roboflow(api_key="Cx14JqJobLYVjeCovFtb")
+    project = rf.workspace("phupha").project("new_cassette")
+    version = project.version(1)
+    dataset = version.download("yolov11")
+                
     model = YOLO('yolo11n.pt')
     model.train(
         data=f"{dataset.location}/data.yaml",
         epochs=300,
         imgsz=640,
         plots=True,
-        resume=True,  # Add this line to resume training
         device=0  # 0 for first GPU, 1 for second, 'cpu' for CPU
     )
 def runtime():
