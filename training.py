067fad217676ada5bcf951452cffdb5bb5ff38f8
Wuttiparb <mizsizterx@gmail.com>
2025-06-25 21:56:16 +0700

    train

--- training.py
+++ training.py
@@ -12,16 +12,22 @@ def main():
 
     rf = Roboflow(api_key="Cx14JqJobLYVjeCovFtb")
     project = rf.workspace("phupha").project("new_cassette")
-    version = project.version(1)
+    version = project.version(3)
     dataset = version.download("yolov11")
                 
-    model = YOLO('yolo11n.pt')
+    model = YOLO('yolo11n.pt')  # Load a pretrained YOLOv11 model
     model.train(
         data=f"{dataset.location}/data.yaml",
         epochs=300,
         imgsz=640,
         plots=True,
-        device=0  # 0 for first GPU, 1 for second, 'cpu' for CPU
+        device=0 , # 0 for first GPU, 1 for second, 'cpu' for CPU
+        resume=False,  # Set to True to resume training from a previous checkpoint
+        save=True,
+        project=r'C:\Users\Lenovo\Desktop\Project tracking\New folder\training-model\data\runs',
+        patience=50,
+        bach=16,  # Batch size
+        name='11n300epV3'  # Name of the training run
     )
 def runtime():
     start = time.time()
